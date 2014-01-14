e1eba12003b39c0d3b23cf05719dbe82621170d3
johnpaulcreates <github@johnpaulsmith.co.uk>
2014-01-14 00:05:03 +0000

    First version

---
+++ Font Tools/frmFonts.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFonts
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFonts))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbCmbSize = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSample = New System.Windows.Forms.TextBox()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.lvwFonts = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.mnuGroups = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CheckToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.btnUncheck = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.mnuGroups.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.DetectUrls = False
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(567, 354)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(93, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Font Size"
+        '
+        'cmbCmbSize
+        '
+        Me.cmbCmbSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbCmbSize.FormattingEnabled = True
+        Me.cmbCmbSize.Items.AddRange(New Object() {"10", "12", "14", "16", "18", "20", "24", "28", "32", "64", "128"})
+        Me.cmbCmbSize.Location = New System.Drawing.Point(150, 1)
+        Me.cmbCmbSize.Name = "cmbCmbSize"
+        Me.cmbCmbSize.Size = New System.Drawing.Size(121, 21)
+        Me.cmbCmbSize.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(277, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Sample Text:"
+        '
+        'txtSample
+        '
+        Me.txtSample.Location = New System.Drawing.Point(352, 2)
+        Me.txtSample.Name = "txtSample"
+        Me.txtSample.Size = New System.Drawing.Size(212, 20)
+        Me.txtSample.TabIndex = 5
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(570, 0)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.btnRefresh.TabIndex = 6
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 35)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.lvwFonts)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.RichTextBox1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(854, 354)
+        Me.SplitContainer1.SplitterDistance = 283
+        Me.SplitContainer1.TabIndex = 7
+        '
+        'lvwFonts
+        '
+        Me.lvwFonts.CheckBoxes = True
+        Me.lvwFonts.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lvwFonts.ContextMenuStrip = Me.mnuGroups
+        Me.lvwFonts.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lvwFonts.GridLines = True
+        Me.lvwFonts.Location = New System.Drawing.Point(0, 0)
+        Me.lvwFonts.Name = "lvwFonts"
+        Me.lvwFonts.Size = New System.Drawing.Size(283, 354)
+        Me.lvwFonts.TabIndex = 1
+        Me.lvwFonts.UseCompatibleStateImageBehavior = False
+        Me.lvwFonts.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Width = 128
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Width = 135
+        '
+        'mnuGroups
+        '
+        Me.mnuGroups.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CheckToolStripMenuItem, Me.ToolStripMenuItem1})
+        Me.mnuGroups.Name = "ContextMenuStrip1"
+        Me.mnuGroups.Size = New System.Drawing.Size(108, 32)
+        '
+        'CheckToolStripMenuItem
+        '
+        Me.CheckToolStripMenuItem.Name = "CheckToolStripMenuItem"
+        Me.CheckToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.CheckToolStripMenuItem.Text = "Check"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(104, 6)
+        '
+        'btnUncheck
+        '
+        Me.btnUncheck.Location = New System.Drawing.Point(0, 0)
+        Me.btnUncheck.Name = "btnUncheck"
+        Me.btnUncheck.Size = New System.Drawing.Size(75, 23)
+        Me.btnUncheck.TabIndex = 8
+        Me.btnUncheck.Text = "Uncheck All"
+        Me.btnUncheck.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 389)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(854, 22)
+        Me.StatusStrip1.TabIndex = 9
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.cmbCmbSize)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.btnUncheck)
+        Me.Panel1.Controls.Add(Me.btnRefresh)
+        Me.Panel1.Controls.Add(Me.txtSample)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(854, 35)
+        Me.Panel1.TabIndex = 10
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'frmFonts
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(854, 411)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmFonts"
+        Me.Text = "Font Preview"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.mnuGroups.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmbCmbSize As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtSample As System.Windows.Forms.TextBox
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents mnuGroups As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents lvwFonts As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnUncheck As System.Windows.Forms.Button
+    Friend WithEvents CheckToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Font Tools/frmFonts.vb
@@ -0,0 +1,401 @@
+﻿Imports Lobo.Common.Data
+
+Public Class frmFonts
+
+
+    Dim LIST_SAMPLE_SIZE As Single = 18
+
+    Dim sDSFilename As String
+    Dim DS As New DataSet
+
+    Private bRedrawInProgress As Boolean = False
+
+
+    Private Sub frmFonts_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        Lobo.Common.Forms.FormHelper.SaveSettings(Me, My.Settings)
+
+        SaveDatabase()
+
+    End Sub
+
+    Private Sub UpdateStatus(ByVal Message As String)
+        ToolStripStatusLabel1.Text = Message
+        Application.DoEvents()
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Me.Cursor = Cursors.WaitCursor
+
+        Lobo.Common.Forms.FormHelper.LoadSettings(Me, My.Settings)
+
+        UpdateStatus("Loading Database...")
+
+        'load the data set
+        sDSFilename = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        sDSFilename = System.IO.Path.Combine(sDSFilename, "Font Preview.ds")
+        If System.IO.File.Exists(sDSFilename) Then
+            DS.Load(sDSFilename)
+        Else
+            CreateDatabase()
+        End If
+
+       initlistview()
+
+
+        'Try
+        '    Lobo.Common.Controls.ListViewHelper.SetGroupCollapseAll(lvwFonts, Lobo.Common.Controls.ListViewHelper.GroupState.COLLAPSIBLE)
+        '    Lobo.Common.Controls.ListViewHelper.SetGroupCollapse(lvwFonts, "_Hidden", Lobo.Common.Controls.ListViewHelper.GroupState.COLLAPSED)
+
+        'Catch ex As Exception
+        '    Debug.Print("oops")
+        'End Try
+
+        Try
+            SaveDatabase()
+        Catch ex As Exception
+            Debug.Assert(False)
+        End Try
+
+        Try
+            ShowFontNames()
+        Catch ex As Exception
+            Debug.Assert(False)
+        End Try
+
+        lvwFonts.EndUpdate()
+
+        UpdateStatus("Ready")
+
+    End Sub
+
+    Private Sub InitListView()
+
+
+        UpdateStatus("Initialising Listview...")
+
+        Me.lvwFonts.BeginUpdate()
+
+        CreateGroup("_Hidden")
+        CreateGroup("Dings")
+        CreateGroup("Script")
+        CreateGroup("Serif")
+        CreateGroup("San-Serif")
+        CreateGroup("Gothic")
+        CreateGroup("Decorative")
+
+        UpdateStatus("Initialising Listview... Done")
+
+        UpdateStatus("Creating Groups...")
+        Me.lvwFonts.BeginUpdate()
+
+        For Each aRow As DataRow In DS.Tables("FontGroups").Rows
+
+            Dim sGroupName As String = ""
+            Try
+                sGroupName = aRow.Item("GroupName").ToString.Trim
+            Catch ex As Exception
+
+            End Try
+            If sGroupName <> "" Then
+                CreateGroup(sGroupName)
+            End If
+
+        Next
+        Me.lvwFonts.EndUpdate()
+
+        UpdateStatus("Creating Groups... Done")
+
+    End Sub
+    Private Function CreateGroup(ByVal GroupName As String) As ListViewGroup
+
+        Dim ThisGroup As ListViewGroup = Lobo.Common.Controls.ListViewHelper.GetGroup(lvwFonts, GroupName)
+
+        'create a menu item for the group
+        Dim mnuItem As New ToolStripMenuItem(GroupName, Nothing)
+                mnuGroups.Items.Add(mnuItem)
+                mnuItem.Tag = ThisGroup
+                AddHandler mnuItem.Click, AddressOf mnuGroupItem_Click
+   
+        Return ThisGroup
+
+    End Function
+
+    Private Sub CreateDatabase()
+
+        DS = New Lobo.Common.Data.DataSet
+        Dim DT As DataTable = DS.CreateTable("FontGroups")
+        DT.CreateColumn("ID", DataTable.eDataType.Integer, True)
+        DT.CreateColumn("GroupName")
+        DT.CreateColumn("FontName")
+        DT.CreateColumn("State", DataTable.eDataType.Boolean)
+
+        DS.AcceptChanges()
+
+    End Sub
+
+    Private Sub SaveDatabase()
+
+        UpdateStatus("Saving Database...")
+
+        DS.Save(sDSFilename)
+
+        UpdateStatus("Saving Database... Done")
+
+    End Sub
+
+    Private Function GetListViewItem(ByVal Key As String, ByVal Name As String) As ListViewItem
+
+        Dim ThisItem As ListViewItem = Nothing
+
+        'first see if the item already exists
+        ThisItem = lvwFonts.Items(Key)
+
+        If ThisItem Is Nothing Then
+            ThisItem = lvwFonts.Items.Add(Key, Name, 0)
+
+            Dim aRow As DataRow = Nothing
+            Dim bFound As Boolean = False
+            For Each MyRow As DataRow In DS.Tables(0).Rows
+                If MyRow.Item("FontName") = Name Then
+                    bFound = True
+                    aRow = MyRow
+                    Exit For
+                End If
+            Next
+
+            If bFound = False Then
+                aRow = DS.Tables(0).NewRow()
+                aRow.Item("FontName") = Name
+                aRow.Item("State") = True
+                DS.Tables(0).Rows.Add(aRow)
+            End If
+
+            ThisItem.Tag = aRow
+            ThisItem.Checked = True
+            'SaveDatabase()
+        End If
+
+
+        Return ThisItem
+
+    End Function
+
+    Private Sub ShowFontNames()
+
+        Me.Cursor = Cursors.WaitCursor
+        ToolStripStatusLabel1.Text = "Showing Fonts..."
+        Dim iCount As Integer = 0
+
+        Application.DoEvents()
+        bRedrawInProgress = True
+
+        lvwFonts.BeginUpdate()
+
+        'enumerate the installed fonts
+        Dim Fonts As New System.Drawing.Text.InstalledFontCollection
+        Dim FontFamilies() As System.Drawing.FontFamily = Fonts.Families
+
+        For Each Font As FontFamily In FontFamilies
+
+            Dim sKey As String = Font.Name.Replace(" ", "_").Replace("#", "")
+            Dim anItem As ListViewItem = GetListViewItem(sKey, Font.Name)
+            Dim FontRow As DataRow = anItem.Tag
+
+            Dim ThisGroup As ListViewGroup = Nothing
+            Dim sGroupName As String = ""
+            Try
+                sGroupName = FontRow.Item("GroupName")
+            Catch ex As Exception
+
+            End Try
+
+            ThisGroup = CreateGroup(sGroupName)
+            If Not ThisGroup Is Nothing Then
+                ThisGroup.Items.Add(anItem)
+            End If
+
+            anItem.Checked = FontRow.Item("State")
+            anItem.UseItemStyleForSubItems = False
+
+            anItem.SubItems.Add(New ListViewItem.ListViewSubItem())
+            Try
+                ' anItem.SubItems(1).Font = New Font(Font.Name, LIST_SAMPLE_SIZE)
+                anItem.SubItems(1).Text = "This is a sample"
+            Catch ex As Exception
+                anItem.SubItems(1).Text = ex.Message
+            End Try
+
+            iCount += 1
+            ToolStripStatusLabel1.Text = "Showing Fonts... " & iCount.ToString & " of " & FontFamilies.Count.ToString
+            Application.DoEvents()
+
+        Next
+
+        bRedrawInProgress = False
+
+        Me.Cursor = Cursors.Default
+        lvwFonts.EndUpdate()
+        UpdateStatus("Showing Fonts... Done")
+
+    End Sub
+
+    Private Sub DrawFonts()
+
+        Me.Cursor = Cursors.WaitCursor
+        Application.DoEvents()
+        bRedrawInProgress = True
+
+        Dim iFontSize As Integer = 12
+
+        If Me.cmbCmbSize.Text <> "" Then
+            iFontSize = Me.cmbCmbSize.Text
+        End If
+
+        RichTextBox1.Clear()
+
+        'first draw the fonts that are not in a group
+        For Each anItem As ListViewItem In lvwFonts.Items
+
+            If anItem.Group Is Nothing Then
+                DrawFont(anItem, iFontSize)
+            End If
+        Next
+
+        For Each aGroup As ListViewGroup In lvwFonts.Groups
+            If aGroup.Name <> "_Hidden" Then
+                Dim iCount As Integer = 0
+                For Each anItem As ListViewItem In aGroup.Items
+                    If anItem.Checked Then
+                        iCount += 1
+                    End If
+                Next
+                If iCount > 0 Then
+                    DrawGroup(aGroup.Name)
+                    For Each anItem As ListViewItem In aGroup.Items
+                        DrawFont(anItem, iFontSize)
+                    Next
+                End If
+
+            End If
+        Next
+
+        bRedrawInProgress = False
+        Me.Cursor = Cursors.Default
+        Application.DoEvents()
+
+    End Sub
+    Private Sub DrawGroup(ByVal GroupName As String)
+        With RichTextBox1
+            .Select(.TextLength, 0)
+            Try
+                .SelectionFont = New Font("Microsoft Sans Serif", 22)
+            Catch ex As Exception
+
+            End Try
+
+            .SelectionColor = Color.Orange
+
+            .AppendText("Group: " & GroupName & vbCrLf)
+
+        End With
+    End Sub
+    Private Sub DrawFont(ByVal anItem As ListViewItem, ByVal iFontSize As Integer)
+
+        If anItem.Checked Then
+            Dim sSample As String = anItem.Text '& vbCrLf
+            If Me.txtSample.Text <> "" Then
+                sSample = Me.txtSample.Text
+            End If
+
+            With RichTextBox1
+                .Select(.TextLength, 0)
+                Try
+                    .SelectionFont = New Font(anItem.Text, iFontSize)
+                Catch ex As Exception
+
+                End Try
+
+                .AppendText(sSample)
+
+                'If Me.txtSample.Text <> "" Then
+                .Select(.TextLength, 0)
+                .SelectionFont = New Font("Microsoft Sans Serif", 8.2)
+                .AppendText(" [" & anItem.Text & "]" & vbCrLf)
+                'End If
+
+            End With
+
+        End If 'if anitem.checked
+
+        Application.DoEvents()
+
+    End Sub
+    Private Sub cmbCmbSize_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbCmbSize.SelectedIndexChanged
+        If cmbCmbSize.SelectedIndex > 0 Then
+            DrawFonts()
+        End If
+    End Sub
+
+    Private Sub btnRefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRefresh.Click
+        DrawFonts()
+    End Sub
+    Private Sub mnuGroupItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        Dim mnu As ToolStripMenuItem = sender
+
+        For Each ListItem As ListViewItem In lvwFonts.SelectedItems
+
+            Dim Row As DataRow = ListItem.Tag
+
+            Row.Item("GroupName") = mnu.Text
+
+            ListItem.Group = CreateGroup(mnu.Text)
+        Next
+
+        SaveDatabase()
+        DrawFonts()
+
+    End Sub
+    Private Sub lvwFonts_ItemChecked(ByVal sender As Object, ByVal e As System.Windows.Forms.ItemCheckedEventArgs) Handles lvwFonts.ItemChecked
+
+        If bRedrawInProgress Then
+            Exit Sub
+        End If
+
+        Dim Item As ListViewItem = e.Item
+        Dim Row As DataRow = Item.Tag
+
+        Row.Item("State") = Item.Checked
+        DrawFonts()
+        SaveDatabase()
+
+    End Sub
+
+    Private Sub btnUncheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUncheck.Click
+        bRedrawInProgress = True
+
+        For Each anItem As ListViewItem In lvwFonts.Items
+            anItem.Checked = False
+            Dim Row As DataRow = anItem.Tag
+            Row.Item("State") = anItem.Checked
+        Next
+        bRedrawInProgress = False
+        DrawFonts()
+        SaveDatabase()
+
+    End Sub
+
+    Private Sub CheckToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckToolStripMenuItem.Click
+
+        bRedrawInProgress = True
+
+        For Each anItem In lvwFonts.SelectedItems
+            anItem.checked = True
+        Next
+        bRedrawInProgress = False
+        DrawFonts()
+        SaveDatabase()
+
+    End Sub
+End Class
